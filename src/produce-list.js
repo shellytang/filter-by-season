33fb55dcb1d6de087c1a81c93745a1d71de02e14
Shelly Tang <shellytang@gmail.com>
2017-12-04 00:35:52 -0800

    loading images for show all and show by filter

--- src/produce-list.js
+++ src/produce-list.js
@@ -5,18 +5,48 @@ class ProduceList extends Component {
   //get list of produce based on selected season and render produce image to component
   render() {
 
-    let produceListBySeason = produceData.filter(item => {
-       return item.season === this.props.selectedSeason;
-    }).map(produce => {
-      return (
-        <div><img src={produce.path} alt={produce.item}></img></div>
-      )
-    });
+    // .map(produce => {
+    //   return (
+    //     <div><img src={produce.path} alt={produce.item}></img></div>
+    //   )
+    // });
+
+    // let allProduce = produceData.map(item => {
+    //   return (
+    //     <div><img src={produce.path} alt={produce.item}></img></div>
+    //   )
+    // });
+
+    // console.log('produceList: ', produceListBySeason);
+    
+    // console.log('what is this: ', loadList());
+
+    let loadList = function(selected) {
+      let allProduceList = produceData;
+      let produceListBySeason = produceData.filter(item => {
+        return item.season === selected;
+      });
+
+      if(selected === '') {
+        return allProduceList.map((produce, index) => {
+          return (
+            <div key={index}><img src={produce.path} alt={produce.item}></img></div>
+          )
+        });
+
+      } else {
+        return produceListBySeason.map((produce, index) => {
+          return (
+            <div key={index}><img src={produce.path} alt={produce.item}></img></div>
+          )
+        });
+      }
+    }
 
     return (
       <div>
         <h2>This is the produce list!</h2>
-        { produceListBySeason }
+         {loadList(this.props.selectedSeason)}
       </div>
     )
   }
