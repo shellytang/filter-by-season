5361f9895bd3bb286f303b3987e33be15781f4ef
Shelly Tang <shellytang@gmail.com>
2017-12-04 09:00:17 -0800

    refactor to remove repeated code

--- src/produce-list.js
+++ src/produce-list.js
@@ -5,44 +5,23 @@ class ProduceList extends Component {
   //get list of produce based on selected season and render produce image to component
   render() {
 
-    // .map(produce => {
-    //   return (
-    //     <div><img src={produce.path} alt={produce.item}></img></div>
-    //   )
-    // });
-
-    // let allProduce = produceData.map(item => {
-    //   return (
-    //     <div><img src={produce.path} alt={produce.item}></img></div>
-    //   )
-    // });
-
-    // console.log('produceList: ', produceListBySeason);
-    
-    // console.log('what is this: ', loadList());
-
     let loadList = function(selected) {
       let allProduceList = produceData;
       let produceListBySeason = produceData.filter(item => {
         return item.season === selected;
       });
 
-      if(selected === '') {
-        return allProduceList.map((produce, index) => {
-          return (
-            <div key={index}><img src={produce.path} alt={produce.item}></img></div>
-          )
-        });
-
-      } else {
-        return produceListBySeason.map((produce, index) => {
+      function _createImg(data) {
+        return data.map((produce, index) => {
           return (
             <div key={index}><img src={produce.path} alt={produce.item}></img></div>
           )
         });
       }
-    }
 
+      return selected === '' ? _createImg(allProduceList) : _createImg(produceListBySeason);
+    }
+    
     return (
       <div>
         <h2>This is the produce list!</h2>
